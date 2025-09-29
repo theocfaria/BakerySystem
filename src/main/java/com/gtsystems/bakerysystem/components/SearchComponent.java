740b11215677aaa25643537a897b07ebae831b29
Gabriel Andrioli <gabrielandfonseca@gmail.com>
2025-09-29 14:33:02 -0300

    bug: tenta arrumar a barra lateral esquerda

--- src/main/java/com/gtsystems/bakerysystem/components/SearchComponent.java
+++ src/main/java/com/gtsystems/bakerysystem/components/SearchComponent.java
@@ -1,23 +1,40 @@
 package com.gtsystems.bakerysystem.components;
 
 import com.gtsystems.bakerysystem.persistence.AccountsPersistanceHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class SearchComponent {
-    public HBox create(Stage stage) throws IOException {
+    public HBox create() throws IOException {
         Map<String, Double> data = AccountsPersistanceHandler.loadData();
 
+        VBox contentBox = new VBox();
+        contentBox.setPrefWidth(250);
+        contentBox.setSpacing(10);
+        contentBox.setPadding(new Insets(10));
+        contentBox.setStyle(
+                "-fx-background-color: #f4f4f4;" +
+                        "-fx-border-color: #c0c0c0;" +
+                        "-fx-border-width: 3px;" +
+                        "-fx-border-radius: 5;"
+        );
+
         TextField searchField = new TextField();
+        searchField.setStyle("-fx-font-size: 22px; -fx-pref-height: 25px; -fx-font-weight: bold");
+
         Label searchTitle = new Label("ACESSAR CONTA");
-        searchTitle.setStyle("-fx-font-size: 15px; -fx-font-weight: bold; -fx-font-family: 'Times New Roman';");
+        searchTitle.setStyle("-fx-font-size: 22px; -fx-font-weight: bold; -fx-font-family: 'Times New Roman';");
+
+        contentBox.getChildren().addAll(searchTitle, searchField);
+
+        HBox externalBox = new HBox();
 
         Label resultLabel = new Label();
         searchField.setOnAction(e -> {
@@ -30,39 +47,11 @@ public class SearchComponent {
             }
         });
 
-        Region spacer = new Region();
-        spacer.setPrefSize(
-                stage.getHeight()/10,
-                stage.getWidth()/10
-        );
+        externalBox.getChildren().addAll(contentBox,resultLabel);
 
-        VBox vbox = new VBox(
-                searchTitle,
-                spacer,
-                searchField,
-                resultLabel
-        );
+        externalBox.setAlignment(Pos.CENTER);
+        externalBox.setSpacing(10);
 
-        vbox.setAlignment(Pos.CENTER);
-
-        Region leftSpacer = new Region();
-        Region rightSpacer = new Region();
-        leftSpacer.setPrefSize(
-                stage.getHeight()/10,
-                stage.getWidth()/10
-        );
-
-        rightSpacer.setPrefSize(
-                stage.getHeight()/10,
-                stage.getWidth()/10
-        );
-
-        HBox.setHgrow(leftSpacer, Priority.ALWAYS);
-        HBox.setHgrow(rightSpacer, Priority.ALWAYS);
-
-        HBox hbox = new HBox(leftSpacer, vbox, rightSpacer);
-        hbox.setAlignment(Pos.CENTER);
-
-        return hbox;
+        return externalBox;
     }
 }
--- src/main/java/com/gtsystems/bakerysystem/screens/HomeScreen.java
+++ src/main/java/com/gtsystems/bakerysystem/screens/HomeScreen.java
@@ -1,9 +1,12 @@
 package com.gtsystems.bakerysystem.screens;
 
 import com.gtsystems.bakerysystem.components.SearchComponent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,14 +14,19 @@ import java.io.IOException;
 public class HomeScreen {
     public void show(Stage stage) throws IOException {
         BorderPane root = new BorderPane();
+        root.setStyle("-fx-background-color: #E8E8E8;");
 
         Label title = new Label("SISTEMA DE CONTAS GT");
         title.setStyle("-fx-font-size: 24px; -fx-font-weight: bold; -fx-font-family: 'Times New Roman';");
-        root.setTop(title);
         BorderPane.setAlignment(title, javafx.geometry.Pos.CENTER);
+        BorderPane.setMargin(title, new Insets(10, 0, 20, 0));
+        root.setTop(title);
 
         SearchComponent searchComponent = new SearchComponent();
-        root.setLeft(searchComponent.create(stage));
+
+        root.setLeft(searchComponent.create());
+        BorderPane.setMargin(searchComponent.create(), new Insets(0, 10, 10, 10));
+        root.setCenter(null);
 
         Scene scene = new Scene(root, 1000, 600);
         stage.setTitle("Tela principal");
