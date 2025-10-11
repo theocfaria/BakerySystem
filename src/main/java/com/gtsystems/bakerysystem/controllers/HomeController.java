b7296194f869011c8e92ada13d225d6d9028597b
Theo Faria <theocarfar@gmail.com>
2025-10-11 14:59:33 -0300

    refactor: make home screen starts with only the search bar visible

--- src/main/java/com/gtsystems/bakerysystem/controllers/HomeController.java
+++ src/main/java/com/gtsystems/bakerysystem/controllers/HomeController.java
@@ -2,26 +2,38 @@ package com.gtsystems.bakerysystem.controllers;
 
 import com.gtsystems.bakerysystem.persistence.AccountsPersistanceHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class HomeController {
+    @FXML public HBox searchContainer = new HBox();
+    @FXML public VBox resultContainer = new VBox();
     @FXML public TextField searchField = new TextField();
     @FXML public Label resultLabel = new Label();
     @FXML public Button searchButton = new Button();
     @FXML public Button payButton = new Button();
     @FXML public Button addSaleButton = new Button();
 
-    public HomeController() throws IOException {}
+    public HomeController() throws IOException {
+        searchContainer.setLayoutX(146);
+    }
 
     @FXML
-    void toggleVisibility(Control element) {
+    void toggleVisibility(VBox element) {
         element.setVisible(!element.isVisible());
+    }
+
+    void showResult() throws IOException {
+        searchContainer.setLayoutY(146);
+        toggleVisibility(resultContainer);
     }
 
     @FXML
@@ -29,6 +41,7 @@ public class HomeController {
         Map<String, Double> data = AccountsPersistanceHandler.loadData();
 
         String key = searchField.getText();
+        showResult();
 
         if (data.containsKey(key)) {
             resultLabel.setText(
